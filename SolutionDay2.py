513b3ff6ed42121e3c2215e0d57ac7cb5ab21755
Ben <benheil@ius.edu>
2017-12-02 22:59:00 -0500

    Update SolutionDay2.py

--- SolutionDay2.py
+++ SolutionDay2.py
@@ -3,14 +3,10 @@ arr = []
 currentRow = []
 for j in range(16):
 	s = input()
-	#print(s)
 	currentRow = s.split('\t')
 	currentRow = [int(i) for i in currentRow]
 	print(currentRow)
 	arr.append(int(max(currentRow)) - int(min(currentRow)))
-	#print(int(max(currentRow)))
-	#print(int(min(currentRow)))
-	#print(arr)
 	currentRow = []
 print(sum(arr))
 	
