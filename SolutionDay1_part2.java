b0e871730a5efa59fbcf3e60bdfdd190c7a82580
Ben <benheil@ius.edu>
2017-12-04 13:19:16 -0500

    Add files via upload

---
+++ SolutionDay1_part2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class SolutionDay1_part2
+{
+	public static int FindAnswer(String s)
+	{
+		int half = s.length() / 2;
+		int count = 0;
+		for(int i = 0;  i < half; i++)
+		{
+			if(s.charAt(i) == s.charAt(i + half))
+			{
+				System.out.println(s.substring(i, i+1));
+				count += 2 * Integer.parseInt(s.substring(i, i+1));
+			}
+		}
+
+		if(s.charAt(half) == s.charAt(s.length() - 1))
+			count += Integer.parseInt(s.substring(half, half+1)); 
+		return count;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner stdin = new Scanner(System.in);
+		String num;
+		num = stdin.next();
+		System.out.println(FindAnswer(num));
+	}
+}
