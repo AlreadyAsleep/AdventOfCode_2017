a938c3bae676c4ff0855d6380375c80abb1383cc
Ben <benheil@ius.edu>
2017-12-04 13:28:33 -0500

    Update SolutionDay3.cpp

--- SolutionDay3.cpp
+++ SolutionDay3.cpp
@@ -3,7 +3,11 @@
 #include <vector>
 using namespace std;
 
-
+/*
+We know the dist from each square will be its root minus 1
+so we only need to start from the closest square below our value
+and walk our way to the value
+*/
 int findClosestSquareFloor(int i)
 {
 	int root_i = (int)floor(sqrt(i));
@@ -17,10 +21,11 @@ int distanceTo1(int num)
 	int dist = (int)sqrt(start) - 1;
 	if(start != num)
 	{
+		//start of the next square
 		start++;
 		dist++;
 
-		for(int k = 0; k < 4; k++)
+		for(int k = 0; k < 4; k++)//each square has 4 sides and since we don't care about direction
 		{
 			for(int i = 0; i < (int)sqrt(start) / 2 && start != num; i++)
 			{
