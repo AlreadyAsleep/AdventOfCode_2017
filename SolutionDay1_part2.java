a11b065d96dfbdbad5b2f6e92f3f4533cb8693f9
Ben <benheil@ius.edu>
2017-12-04 13:21:42 -0500

    Update SolutionDay1_part2.java

--- SolutionDay1_part2.java
+++ SolutionDay1_part2.java
@@ -3,18 +3,18 @@ public class SolutionDay1_part2
 {
 	public static int FindAnswer(String s)
 	{
-		int half = s.length() / 2;
+		int half = s.length() / 2;//index of the halfway point
 		int count = 0;
 		for(int i = 0;  i < half; i++)
 		{
 			if(s.charAt(i) == s.charAt(i + half))
 			{
 				System.out.println(s.substring(i, i+1));
-				count += 2 * Integer.parseInt(s.substring(i, i+1));
+				count += 2 * Integer.parseInt(s.substring(i, i+1));// because each occurence counts twice
 			}
 		}
 
-		if(s.charAt(half) == s.charAt(s.length() - 1))
+		if(s.charAt(half) == s.charAt(s.length() - 1))//finally check the char at the halfway point against the last char
 			count += Integer.parseInt(s.substring(half, half+1)); 
 		return count;
 	}
