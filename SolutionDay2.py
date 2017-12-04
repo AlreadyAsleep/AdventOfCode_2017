30e0fd74040852f7a4ea914bca4c48655e9e548d
Ben <benheil@ius.edu>
2017-12-04 13:22:47 -0500

    Update SolutionDay2.py

--- SolutionDay2.py
+++ SolutionDay2.py
@@ -2,11 +2,11 @@ from math import *
 arr = []
 currentRow = []
 for j in range(16):
-	s = input()
-	currentRow = s.split('\t')
-	currentRow = [int(i) for i in currentRow]
+	s = input()#read the input
+	currentRow = s.split('\t')#convert it to a list
+	currentRow = [int(i) for i in currentRow]#cast each element to an int
 	print(currentRow)
-	arr.append(int(max(currentRow)) - int(min(currentRow)))
+	arr.append(int(max(currentRow)) - int(min(currentRow)))#add the checksum to a list
 	currentRow = []
-print(sum(arr))
+print(sum(arr))#return the checksum
 	
