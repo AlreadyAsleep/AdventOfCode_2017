314e57581fb0b68966752ad7245fb277f1624366
Ben <benheil@ius.edu>
2017-12-04 13:25:27 -0500

    Update SolutionDay2_part2.py

--- SolutionDay2_part2.py
+++ SolutionDay2_part2.py
@@ -2,14 +2,15 @@ from math import *
 arr = []
 currentRow = []
 for j in range(16):
-	s = input()
-	currentRow = s.split('\t')
-	currentRow = [int(i) for i in currentRow]
+	s = input()#read the input
+	currentRow = s.split('\t')#convert it to a list
+	currentRow = [int(i) for i in currentRow]#cast each element to an int
 	stop = False
 	for i in range(16):
 		for k in range(16):
-			if(i == k):
+			if(i == k):#b/c n % n always == 0
 				continue
+			#find the first evenely divisible pair
 			if ( currentRow[i] % currentRow[k] == 0):
 				arr.append(currentRow[i] // currentRow[k])
 				stop = True
@@ -18,8 +19,8 @@ for j in range(16):
 				arr.append(currentRow[k] // currentRow[i])
 				stop = True
 				break
-		if stop:
+		if stop:#stated only one occurence per line in problem desc.
 			break
 	currentRow = []
-print(sum(arr))
+print(sum(arr))#print the sum of all div pairs
 	
